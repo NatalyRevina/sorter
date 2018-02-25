905e6081f31d7fbfb3a64113e9a998212e7141c0
Revina Nataliia <natalyrevina@gmail.com>
2018-02-25 22:34:03 +0300

    implemented Sorter class

--- src/index.js
+++ src/index.js
@@ -1,31 +1,38 @@
 class Sorter {
-  constructor() {
-    // your implementation
-  }
+	constructor() {
+		this.arr = [];
+		this.length = 0;
+		this.compareFunction = (left, right) => left - right;
+	}
 
-  add(element) {
-    // your implementation
-  }
+	add(element) {
+		this.length++;
+		this.arr.push(element);	
+	}
 
-  at(index) {
-    // your implementation
-  }
+	at(index) {
+		return this.arr[index];
+	}
 
-  get length() {
-    // your implementation
-  }
+	toArray() {
+		return(this.arr);
+	}
 
-  toArray() {
-    // your implementation
-  }
+	sort(indices) {
+		indices.sort();
+		for(var i = 0; i < indices.length; i++){
+			for(var j = 0; j < indices.length - 1; j++){
+				var currentNumber = this.arr[indices[j]];
+				if(this.compareFunction(currentNumber, this.arr[indices[j + 1]]) > 0){
+					this.arr[indices[j]] = this.arr[indices[j + 1]];
+					this.arr[indices[j + 1]]  = currentNumber;
+				}
+			}
+		}
+	}
 
-  sort(indices) {
-    // your implementation
-  }
-
-  setComparator(compareFunction) {
-    // your implementation
-  }
+	setComparator(compareFunction) {
+		this.compareFunction = compareFunction;
+	}
 }
-
 module.exports = Sorter;
